a1041139e4816bb9bfc9887c1704869988456c44
MarceloAGD <marcelo.guerra@alumnos.ucn.cl>
2023-06-24 01:17:46 -0400

    update gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,8 @@ import { GraphQLModule } from '@nestjs/graphql';
         supergraphSdl: new IntrospectAndCompose ({
           subgraphs:[
             {name: 'users', url: 'http://localhost:4001/graphql'},
-            {name: 'playlists', url: 'http://localhost:4002/graphql'}
+            {name: 'playlists', url: 'http://localhost:4002/graphql'},
+            {name: 'movies', url: 'http://localhost:4003/graphql'},
           ]
         })
       }
